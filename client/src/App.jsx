d54fdaa7fa0715ce8d9a39ea653d507f54ec5396
ShiroNhL2077 <hosni.trabelsi@rmsoftware.de>
2024-01-31 16:14:20 +0100

    setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home/Home'
+import RangeSlider from './components/rangeslider/RangeSlider'
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
